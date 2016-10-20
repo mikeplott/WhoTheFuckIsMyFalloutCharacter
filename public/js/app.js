141b58896ab8d6aa68a4f6fb949befc8a55561d6
J <jordanmledford@gmail.com>
2016-10-20 16:54:25 -0400

    indentation

--- public/js/app.js
+++ public/js/app.js
@@ -2,21 +2,30 @@
 /* Show Modal of Options */
 $('#myModal').modal('show');
 
+
+/*API Response and Iteration*/
+
+var serverResponse = function(data){
+  for (var i = 0; i < data.length; i++) {
+    return data;
+  }
+}
+
 /* Dynamic content blocks*/
 //
 var falloutContent = ""
   falloutContent += '<div class="text-center">';
-  falloutContent +=    "<h1> My SPECIAL is " + serverResponse[i] + "</h1>";
+  falloutContent +=    "<h1> My SPECIAL is " + serverResponse[2] + "</h1>";
   falloutContent +=    "<h2> and I am a " + serverResponse[3] + "with " + serverResponse[4] + "</h2>";
-  falloutContent +=       "<h1> it works!! </h1>";
+  falloutContent +=    "<h1> it works!! </h1>";
   falloutContent += "</div>";
 
 var skyrimContent = ""
   skyrimContent += '<div class="text-center">';
-  skyrimContent += "<h1> i love waka flocka</h1>";
-  skyrimContent += "<h1> Your race is " + serverResponse.race + ".</h1>";
-  skyrimContent += "<h2> You are a " + PersonJSON  + "with " + fact1JSON "</h2>"
-  skyrimContent += "<h2> and a " + fact2JSON + "</h2>"
+  skyrimContent +=   "<h1> i love waka flocka</h1>";
+  skyrimContent +=   "<h1> Your race is " + serverResponse.race + ".</h1>";
+  // skyrimContent += "<h2> You are a " + serverResponse[1]  + "with " + serverResponse[2] "</h2>"
+  skyrimContent +=   "<h2> and a " + serverResponse[3] + "</h2>"
   skyrimContent += "</div>";
 
 /*jQuery Functions & Calls*/
