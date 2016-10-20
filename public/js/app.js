cfdb4036b99924b288a7adb2fa91c1b87a063d99
J <jordanmledford@gmail.com>
2016-10-20 13:40:28 -0400

    added modal removal routing

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,43 @@ console.log('ready to go')
 console.log($)
 console.log(_)
 console.log(Backbone)
-/* Show Modal of Choices */ 
+/* Show Modal of Choices */
 $('#myModal').modal('show');
+
+/* Dynamic content blocks*/
+// var skyrimContent = ""
+//   skyrimContent +=
+//   skyrimContent +=
+//   skyrimContent +=
+//   skyrimContent +=
+//   skyrimContent +=
+//
+// var falloutContent = ""
+//   falloutContent +=
+//   falloutContent +=
+//   falloutContent +=
+//   falloutContent +=
+//   falloutContent +=
+//   falloutContent +=
+/*Backbone routing */
+// var AppRouter = Backbone.Router.extend({
+//     routes: {
+//         "fallout": "falloutContent"
+//         "skyrim": "skyrimContent"
+//     }
+// });
+var clickListener = function(){
+  $( "#fallout-button" || "#skyrim-button" ).click(function() {
+  $('#myModal').modal('hide');
+  });
+}
+// Initiate the router
+// var app_router = new AppRouter;
+//
+// app_router.on('route:defaultRoute', function(actions) {
+//     alert(actions);
+// });
+// Backbone.history.start();
+
+// $.getJSON("").then(injectFallout)
+// $.getJSON("").then(injectFallout)
