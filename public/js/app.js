dda1ad9053c3cb9e4c1d687b4cb0a85b7702a6fa
J <jordanmledford@gmail.com>
2016-10-20 16:19:14 -0400

    fixed api

--- public/js/app.js
+++ public/js/app.js
@@ -5,33 +5,38 @@ $('#myModal').modal('show');
 /* Dynamic content blocks*/
 //
 var falloutContent = ""
-  falloutContent += '<div id="fallout">'
+  falloutContent += '<div class="text-center">';
   // falloutContent +=    "<h1> My SPECIAL is " + specialJSON + "</h1>"
   // falloutContent +=    "<h2> and I am a " + personJSON + "with " + fact1JSON + "</h2>"
-  falloutContent +=       "<h1> it works!! </h1>"
-  falloutContent += "</div>"
+  falloutContent +=       "<h1> it works!! </h1>";
+  falloutContent += "</div>";
+
 var skyrimContent = ""
-  skyrimContent += '<div id="skyrim">'
-  skyrimContent += "<h1> i love waka flocka</h1>"
+  skyrimContent += '<div class="text-center">';
+  skyrimContent += "<h1> i love waka flocka</h1>";
 //   skyrimContent += "<h1> My Race is " + raceJSON + "</h1>"
 //   skyrimContent += "<h2> and I am a " + PersonJSON  + "with " + fact1JSON "</h2>"
 //   skyrimContent += "<h2> and a " + fact2JSON + "</h2>"
-  skyrimContent += "</div>"
+  skyrimContent += "</div>";
 
 /*jQuery Functions & Calls*/
 $( "#fallout-button" ).click(function() {
   $('#myModal').modal('hide');
-  // $getJSON("mikesurl").then(injectFallout)
+  // $.getJSON("mikesurl").then(injectFallout)
   $( "#fallout" ).append( falloutContent );
 });
 $( "#skyrim-button" ).click(function() {
   $('#myModal').modal('hide');
-  // $getJSON("mikesurl").then(injectFallout)
+  // $.getJSON("mikesurl").then(injectFallout)
   $( "#skyrim" ).append( skyrimContent );
 });
-$.getJSON("http://localhost:4567/skyrim").then(loggerStuff)
-$.getJSON("").then()
+
+/*API Test*/
 
 var loggerStuff = function(data){
-  console.log(data)
+  var serverResponse = data
+  console.log(serverResponse);
 }
+
+$.getJSON("http://localhost:4567/skyrim").then(loggerStuff)
+$.getJSON("http://localhost:4567/fallout").then(loggerStuff)
