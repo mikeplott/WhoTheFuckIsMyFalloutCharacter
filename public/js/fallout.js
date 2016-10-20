0251a40cd74448ae1ae3e5f1f38fdf9c3fbb1c51
Jordan Wicker <Mike_company@yahoo.com>
2016-10-20 16:06:52 -0400

    adding backstories

---
+++ public/js/fallout.js
@@ -0,0 +1,24 @@
+// RANDOM BACKSTORIES FOR FALLOUT
+//BACKSTORY 1
+
+You come from the Capital Wasteland.  After bouncing around many different caravans and settlments, you finally find a home in the Commonwealth.  Everything was fine until your spouse vanished one night at the hands of the Institute.
+
+//BACKSTORY 2
+
+You come from the Pitt.  Your years of running with a raider gang have turned you into a hardened killer. But after a raid went wrong and your gang boss was killed, you were framed for his death and went on the run to escape the bounty they put on your head.
+
+//BACKSTORY 3
+
+You come from Ronto. You were part of a local settlment and served on its government.  But after some raiders came and killed your wife, you now spend your days hunting them down.
+
+//BACKSTORY 4
+
+You come from the NCR.  You were part of the NCR Army but after a battle with the Legion, you were seperated from your unit, only to find them all dead. Since then, you have just been wandering through the desert trying to find home.
+
+//BACKSTORY 5
+
+You come from Vault 13.  You were sent outside to try and establish contact with anyone who may still be alive.  Your vault was isolated for 200 years so you had no knowledge of the outside world and this whole world is very foreign to you.
+
+//BACKSTORY 6
+
+You are from before the bombs fell.  Radiation has kept you alive for over 200 years.  You spend your days wondering when it will end.
