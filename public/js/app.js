2db323cd2861a8360f41397143bb1e2503f59c93
J <jordanmledford@gmail.com>
2016-10-21 13:23:07 -0400

    moved api query inside event listener

--- public/js/app.js
+++ public/js/app.js
@@ -5,15 +5,14 @@ $('#myModal').modal('show');
 /*API Response and Iteration*/
 
 var newContentConstructor = function(data){
-    var falloutChar = data[1]
-    console.log(data[1])
+    console.log(data)
+    var falloutChar = data[0]
     for (var i = 0; i < falloutChar.length; i++) {
     var slicedData = falloutChar.slice(',');
     slicedData.replace('{', '');
     slicedData.replace('}', '');
     console.log(slicedData);
     }
-
     var falloutCharacters = slicedData;
   if (typeof falloutCharacters === "object"){
     return falloutCharacters;
@@ -22,19 +21,11 @@ var newContentConstructor = function(data){
   }
 }
 
-/* Dynamic content blocks*/
-// var skyrimContent = ""
-//   skyrimContent += '<div class="text-center">';
-//   skyrimContent +=   "<h1> i love waka flocka</h1>";
-//   skyrimContent +=   "<h1> My race is " + skyrimCharacters[race] + ".</h1>";
-//   // skyrimContent += "<h2> You are a " + serverResponse[1]  + "with " + serverResponse[2] "</h2>"
-//   skyrimContent +=   "<h2> and a " + SkyrimCharacters[3] + "</h2>"
-//   skyrimContent += "</div>";
-
 /*jQuery Functions & Calls*/
 
 $( "#fallout-button" ).click(function() {
   $('#myModal').modal('hide');
+  $.getJSON("http://localhost:4567/fallout").then(newContentConstructor)
   $( "#fallout" ).append( falloutContent );
 });
 $( "#skyrim-button" ).click(function() {
@@ -42,5 +33,4 @@ $( "#skyrim-button" ).click(function() {
   $( "#skyrim" ).append( skyrimContent );
 });
 
-$.getJSON("http://localhost:4567/skyrim").then(newContentConstructor)
-$.getJSON("http://localhost:4567/fallout").then(newContentConstructor)
+// $.getJSON("http://localhost:4567/skyrim").then(newContentConstructor)
